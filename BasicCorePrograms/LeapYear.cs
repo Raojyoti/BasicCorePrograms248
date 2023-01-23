d110364fa1f66840406c5b076761b040920c76d5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-23 14:32:07 +0530

    [Jyoti Rao] Print year is leap year or not

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    /// LeapYear class is create to check year is leap year or not
+    /// </summary>
+    public class LeapYear
+    {
+        public static void CheckLeapYear()
+        {
+            Console.WriteLine("Please Enter four digit number for year");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if(year % 4==0)
+            {
+                Console.WriteLine("{0} is Leap year", year);
+            }
+            else if(year % 400==0)
+            {
+                Console.WriteLine("{0} is Leap year", year);
+            }
+            else if(year % 100==0)
+            {
+                Console.WriteLine("{0} is not a Leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a Leap year", year);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -7,8 +7,29 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Basic Core Programs");
-            FlipCoin.FindPercentageOfFlipCoin();
-        
+            while(true)
+            {
+                Console.WriteLine("1.FindPercentageOfFlipCoin\n2.CheckLeapYear\n11.Exit\n");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.Clear();
+                        FlipCoin.FindPercentageOfFlipCoin();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        LeapYear.CheckLeapYear();
+                        break;
+                    case 11:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Please select only given options only");
+                        break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
