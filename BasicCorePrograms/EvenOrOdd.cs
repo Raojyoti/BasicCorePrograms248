45c2212cb6ff15899c433d3ab8dad4a9e0e29460
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-23 23:40:57 +0530

    [Jyoti Rao] check whether a number is Even or Odd

---
+++ BasicCorePrograms/EvenOrOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    /// EvenOrOdd class is created to check whether a number is Even or Odd
+    /// </summary>
+    public class EvenOrOdd
+    {
+        static int number;
+        public static void CheckNumberIsEvenOrOdd()
+        {
+            Console.WriteLine("Please enter number to check even or odd:");
+            number=Convert.ToInt32(Console.ReadLine());
+            if(number % 2==0)
+            {
+                Console.WriteLine("{0} is even number",number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is odd number",number);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCorePrograms
             Console.WriteLine("Welcome to the Basic Core Programs");
             while(true)
             {
-                Console.WriteLine("1.FindPercentageOfFlipCoin\n2.CheckLeapYear\n3.FindPowerOfTwo\n4.PrintHarmonicNumber\n5.PrintPrimeFactors\n6.ComputeQuotientAndRemainder\n7.SwapTwoNumbers\n11.Exit\n");
+                Console.WriteLine("1.FindPercentageOfFlipCoin\n2.CheckLeapYear\n3.FindPowerOfTwo\n4.PrintHarmonicNumber\n5.PrintPrimeFactors\n6.ComputeQuotientAndRemainder\n7.SwapTwoNumbers\n8.CheckNumberIsEvenOrOdd\n11.Exit\n");
                 int option=Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -40,6 +40,10 @@ namespace BasicCorePrograms
                     case 7:
                         Console.Clear();
                         Swap.SwapTwoNumbers();
+                        break;
+                    case 8:
+                        Console.Clear();
+                        EvenOrOdd.CheckNumberIsEvenOrOdd();
                         break;
                     case 11:
                         Console.Clear();
