7cb5bf61d20f871134c5481a2e7dce4904b52e97
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-23 15:54:00 +0530

    [Jyoti Rao] Print Nth Harmonic Number

---
+++ BasicCorePrograms/HarmonicNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    /// <summary>
+    /// HarmonicNumber class is create to print Nth harmonic numbers
+    /// </summary>
+    public class HarmonicNumber
+    {
+        static int number;
+        static double result = 0,value=0,i;
+        public static void PrintHarmonicNumber()
+        {
+            System.Console.WriteLine("Please Enter number until you want to print Harmonic Values");
+            number= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Harmonic number series are: ");
+            for (i=1; i<=number; i++)
+            {
+                value=1/i;
+                result = result + (1 / i);
+                Console.WriteLine("1/{0}  => {1} ",i,value);
+            }
+            Console.WriteLine("\nSum of Nth Harmonic Numbers: " + result);
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -9,7 +9,7 @@ namespace BasicCorePrograms
             Console.WriteLine("Welcome to the Basic Core Programs");
             while(true)
             {
-                Console.WriteLine("1.FindPercentageOfFlipCoin\n2.CheckLeapYear\n3.FindPowerOfTwo\n11.Exit\n");
+                Console.WriteLine("1.FindPercentageOfFlipCoin\n2.CheckLeapYear\n3.FindPowerOfTwo\n4.PrintHarmonicNumber\n11.Exit\n");
                 int option=Convert.ToInt32(Console.ReadLine());
                 switch(option)
                 {
@@ -24,6 +24,10 @@ namespace BasicCorePrograms
                     case 3:
                         Console.Clear();
                         PowerOfTwo.FindPowerOfTwo();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        HarmonicNumber.PrintHarmonicNumber();
                         break;
                     case 11:
                         Console.Clear();
